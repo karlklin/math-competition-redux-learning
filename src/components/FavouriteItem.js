4ed4761c3cc6a30a127a4f31941b590401180828
karol.pawelski <karol.pawelski@cognifide.com>
2020-04-02 21:41:58 +0200

    mobx state + observer note

--- src/components/FavouriteItem.js
+++ src/components/FavouriteItem.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { correctAnswer, isCorrect } from "../services/competitionHelper";
 import {observer} from "mobx-react";
 
+
+// Here is interesting. Item is not observable and we could observe it and we get notification about modification.
+// Note that it is working for objects and doesn't work for primitive values (e.g. PercentageTotalResult)
 export const FavouriteItem = observer(({item}) => {
     const correct = isCorrect(item);
 
