70a92fc2cc73292968b748e8f572fdb83d5a8227
karol.pawelski <karol.pawelski@cognifide.com>
2020-05-29 15:59:16 +0200

    mobx start | api + loading

--- src/components/CompetitionManager.js
+++ src/components/CompetitionManager.js
@@ -6,24 +6,26 @@ import {HistoryLog} from './HistoryLog';
 import {Favourites} from "./Favourites";
 import {Loading} from "./Loading";
 import {PageTitle} from "./PageTitle";
+import {useApi} from '../services/api';
 
 const difficulties = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export function CompetitionManager() {
+    const [api, isLoading] = useApi();
 
     const [difficulty, setDifficulty] = useState(5);
     const updateDifficulty = num => e => {
         setDifficulty(num);
         e.preventDefault();
-    }
+    };
 
     const [answers, addAnswer, deleteAnswer] = useAnswers([
         {id: 1, a: 5, b: 10, operator: '+', answer: 15},
         {id: 2, a: 5, b: 10, operator: '-', answer: 10},
         {id: 3, a: 5, b: 10, operator: '*', answer: 10},
-    ]);
+    ], api);
 
-    const [favourites, addLike, removeLike] = useFavourites();
+    const [favourites, addLike, removeLike] = useFavourites(api);
 
     return (
         <div>
@@ -48,22 +50,35 @@ export function CompetitionManager() {
                             unlikeAnswer={removeLike}/>
                 <Favourites items={favourites}/>
             </div>
-            <Loading/>
+            <Loading isLoading={isLoading}/>
             <PageTitle answers={answers}/>
         </div>
     );
 }
 
-const useAnswers = (initial = []) => {
+const useAnswers = (initial = [], api) => {
     const [answers, setAnswers] = useState(initial);
-    const addAnswer = answer => setAnswers([...answers, answer]);
-    const deleteAnswer = id => setAnswers(answers.filter(item => item.id !== id));
+    const addAnswer = async answer => {
+        const resultAnswer = await api.addAnswer(answer);
+        setAnswers([...answers, resultAnswer]);
+    };
+    const deleteAnswer = async id => {
+        const resultId = await api.deleteAnswer(id);
+        setAnswers(answers.filter(item => item.id !== resultId));
+    };
     return [answers, addAnswer, deleteAnswer];
 };
 
-const useFavourites = (initial = []) => {
+const useFavourites = (api, initial = []) => {
     const [favourites, setFavourite] = useState(initial);
-    const addLike = answer => setFavourite([...favourites, answer]);
-    const removeLike = id => setFavourite(favourites.filter(item => item.id !== id));
+    const addLike = async answer => {
+        const resultAnswer = await api.like(answer);
+        setFavourite([...favourites, resultAnswer]);
+    };
+    const removeLike = async id => {
+        const resultId = await api.unlike(id);
+        setFavourite(favourites.filter(item => item.id !== resultId));
+    };
+
     return [favourites, addLike, removeLike];
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,29 @@
+import React, {useState} from 'react';
+
 export const api = {
-    addAnswer: () => new Promise(resolve => setTimeout(resolve, 1000)),
-    deleteAnswer: () => new Promise(resolve => setTimeout(resolve, 1000)),
-    like: () => new Promise(resolve => setTimeout(resolve, 1000)),
-    unlike: () => new Promise(resolve => setTimeout(resolve, 1000)),
+    addAnswer: answer => new Promise(resolve => setTimeout(() => resolve(answer), 1000)),
+    deleteAnswer: id => new Promise(resolve => setTimeout(() => resolve(id), 1000)),
+    like: answer => new Promise(resolve => setTimeout(() => resolve(answer), 1000)),
+    unlike: answer => new Promise(resolve => setTimeout(() => resolve(answer), 1000)),
     updateAnswer: () => new Promise(resolve => setTimeout(resolve, 1000)),
+};
+
+export const useApi = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const runWithLoading = apiFn => async data => {
+        setIsLoading(true);
+        const result = await apiFn(data);
+        setIsLoading(false);
+        return result;
+    };
+
+    const withLoading = {
+        addAnswer: runWithLoading(api.addAnswer),
+        deleteAnswer: runWithLoading(api.deleteAnswer),
+        like: runWithLoading(api.like),
+        unlike: runWithLoading(api.unlike)
+    };
+
+    return [withLoading, isLoading];
 };
