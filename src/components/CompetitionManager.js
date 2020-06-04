994595d2aae0494e78e5ffd3848dd585703c0c5a
karol.pawelski <karol.pawelski@cognifide.com>
2020-06-04 21:31:10 +0200

    with-mobx--start2 | loading as local state

--- src/components/CompetitionManager.js
+++ src/components/CompetitionManager.js
@@ -6,12 +6,12 @@ import {HistoryLog} from './HistoryLog';
 import {Favourites} from "./Favourites";
 import {Loading} from "./Loading";
 import {PageTitle} from "./PageTitle";
-import {useApi} from '../services/api';
+import {api} from '../services/api';
 
 const difficulties = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const CompetitionManager = () => {
-    const [api, isLoading] = useApi();
+    const [loading, setLoading] = useState([]);
 
     const [difficulty, setDifficulty] = useState(5);
     const updateDifficulty = num => e => {
@@ -23,9 +23,9 @@ export const CompetitionManager = () => {
         {id: 1, a: 5, b: 10, operator: '+', answer: 15},
         {id: 2, a: 5, b: 10, operator: '-', answer: 10},
         {id: 3, a: 5, b: 10, operator: '*', answer: 10},
-    ], api);
+    ], loading, setLoading);
 
-    const [favourites, addLike, removeLike] = useFavourites(api);
+    const [favourites, addLike, removeLike] = useFavourites([], loading, setLoading);
 
     return (
         <div>
@@ -50,34 +50,42 @@ export const CompetitionManager = () => {
                             unlikeAnswer={removeLike}/>
                 <Favourites items={favourites}/>
             </div>
-            <Loading isLoading={isLoading}/>
+            <Loading isLoading={loading.length > 0}/>
             <PageTitle answers={answers}/>
         </div>
     );
-}
+};
 
-const useAnswers = (initial = [], api) => {
+const useAnswers = (initial, loading, setLoading) => {
     const [answers, setAnswers] = useState(initial);
     const addAnswer = async answer => {
+        setLoading([...loading, true]);
         const resultAnswer = await api.addAnswer(answer);
         setAnswers([...answers, resultAnswer]);
+        setLoading(loading.slice(0, loading.length-2));
     };
     const deleteAnswer = async id => {
+        setLoading([...loading, true]);
         const resultId = await api.deleteAnswer(id);
         setAnswers(answers.filter(item => item.id !== resultId));
+        setLoading(loading.slice(0, loading.length-2));
     };
     return [answers, addAnswer, deleteAnswer];
 };
 
-const useFavourites = (api, initial = []) => {
+const useFavourites = (initial, loading, setLoading) => {
     const [favourites, setFavourite] = useState(initial);
     const addLike = async answer => {
+        setLoading([...loading, true]);
         const resultAnswer = await api.like(answer);
         setFavourite([...favourites, resultAnswer]);
+        setLoading(loading.slice(0, loading.length-2));
     };
     const removeLike = async id => {
+        setLoading([...loading, true]);
         const resultId = await api.unlike(id);
         setFavourite(favourites.filter(item => item.id !== resultId));
+        setLoading(loading.slice(0, loading.length-2));
     };
 
     return [favourites, addLike, removeLike];
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,3 @@
-import {useState} from 'react';
-
 export const api = {
     addAnswer: answer => new Promise(resolve => setTimeout(() => resolve(answer), 1000)),
     deleteAnswer: id => new Promise(resolve => setTimeout(() => resolve(id), 1000)),
@@ -8,22 +6,3 @@ export const api = {
     updateAnswer: (id, newAnswer) => new Promise(resolve => setTimeout(() => resolve(id), 1000)),
 };
 
-export const useApi = () => {
-    const [isLoading, setIsLoading] = useState(false);
-
-    const callApi = apiFn => async data => {
-        setIsLoading(true);
-        const result = await apiFn(data);
-        setIsLoading(false);
-        return result;
-    };
-
-    const withLoading = {
-        addAnswer: callApi(api.addAnswer),
-        deleteAnswer: callApi(api.deleteAnswer),
-        like: callApi(api.like),
-        unlike: callApi(api.unlike)
-    };
-
-    return [withLoading, isLoading];
-};
