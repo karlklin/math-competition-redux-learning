5aa73aaea22695a7e867f71ba2bf8f13652b2d91
karol.pawelski <karol.pawelski@cognifide.com>
2020-06-25 16:00:25 +0200

    with-mobx--start-part4 | import

--- src/components/CompetitionManager.js
+++ src/components/CompetitionManager.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Header} from './Header';
 import {Competition} from './Competition';
 import {PercentageHistory} from './PercentageHistory';
